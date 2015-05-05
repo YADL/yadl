d86bf5e8a82aa9e61499840884f127ca698fe931
Luis Pabón <lpabon@redhat.com>
2015-05-05 03:12:10 +0000

    Add cmockery support

---
+++ src/delete_test.c
@@ -0,0 +1,18 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <inttypes.h>
+#include <cmockery/cmockery.h>
+
+// A test case that does nothing and succeeds.
+static void null_test_success(void **state) {
+    (void) state;
+}
+
+int main(void) {
+    const UnitTest tests[] = {
+        unit_test(null_test_success),
+    };
+
+    return run_tests(tests, "delete_test");
+}
